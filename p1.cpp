a1dfbdcfedbf020b9234bdd5ba4904ad25166ac2
nvasiu <46430801+nvasiu@users.noreply.github.com>
2019-11-19 21:02:28 -0800

    Update p1.cpp
    3d update

--- p1.cpp
+++ p1.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <math.h>
 #include <time.h>
 #include <GL/glut.h>
@@ -14,21 +15,34 @@
 #define LEFT 75
 #define RIGHT 77
 
+using namespace std;
+
 bool Playing = true; // Whether the game is over or not
 
-float lastTime = 0;
-float currentTime = 0;
-float timeInterval = 200;
+float lastTime = 0; 
+float currentTime = 0; // The program records the current time and the time of the last game cycle
+float timeInterval = 5000; // The game advances when the time between cycles is greater than this
 
-double camAngle = 0;
-double armLowerAngle = 0;
-double armUpperAngle = 0;
+double camAngle = 0; // The position of the camera
+
+double armLowerLength = 12; // Size, position, angle of the lower part of the robot arm
+double armLowerX = -3;
+double armLowerY = 2;
+double armLowerAngle = 90;
+
+double armUpperLength = 10; // Size, position, angle of the upper part of the robot arm
+double armUpperX = -3;
+double armUpperY = 14;
+double armUpperAngle = -90;
+
+double armPieceX, armPieceY; // The x and y position of the game piece that the robot arm is holding
 
 Board *boardP;
 Piece *pieceP;
 int currentPiece[5][5]; // Holds tiles of the current piece
 int rotatedPiece[5][5]; // Holds tiles of the current piece rotated CCW 90 degrees, for checking if the rotation causes a collision
 int pieceX, pieceY; // The x and y coordinates of the current piece
+
 int input_key; // The value of the key input by the user
 bool newPiece = false; // Flag for the creation of a new piece, triggered when the current piece lands
 
@@ -72,8 +86,45 @@ void createRotatedPiece() { // Fills a temp variable with the current piece's ti
 	
 }
 
+void drawTimer() { // Draws a count down of the time until the game piece is dropped
+
+	int timeLeft = round((timeInterval - (currentTime - lastTime))/1000.0);
+	string str1 = to_string(timeLeft);
+
+	if (timeLeft > 1) {
+		glColor3f(0.0, 0.0, 0.0);
+	} else {
+		glColor3f(1.0, 0.0, 0.0);
+	}
+
+	int len = str1.length();
+
+	glPushMatrix();
+	glLineWidth(10);
+	glTranslatef(12,18,0);
+	glScalef(0.03, 0.03, 0.03);
+	for (int i = 0; i < len; i++) {
+		glutStrokeCharacter(GLUT_STROKE_ROMAN, str1[i]);
+	}
+	glLineWidth(1);
+	glPopMatrix();
+	
+}
+
 void drawPiece() { // Draws the current piece on the window, the current piece is not yet added to the board
 
+	glPushMatrix();
+	
+	glTranslatef(-6, 22, 0);
+
+	glTranslatef(3.5, 3.5, 0);
+	glRotatef(-armUpperAngle+90, 0, 0, 1);
+	glTranslatef(-3.5, -3.5, 0);
+
+	glTranslatef(3.5, 3.5, 0);
+	glRotatef(-armLowerAngle, 0, 0, 1);
+	glTranslatef(-3.5, -3.5, 0);
+	
 	for (int y = 0; y < 5; y++) {
 		for (int x = 0; x < 5; x++) {
 			if (currentPiece[y][x] != 0) {
@@ -92,45 +143,120 @@ void drawPiece() { // Draws the current piece on the window, the current piece i
 				}
 
 				glBegin(GL_POLYGON);
-				glVertex3f( pieceX+x+2, pieceY+y+1, 0 );
-				glVertex3f( pieceX+x+2, pieceY+y+2, 0 );
-				glVertex3f( pieceX+x+1, pieceY+y+2, 0 );
-				glVertex3f( pieceX+x+1, pieceY+y+1, 0 );
+				glVertex3f( x+2, y+1, 0 );
+				glVertex3f( x+2, y+2, 0 );
+				glVertex3f( x+1, y+2, 0 );
+				glVertex3f( x+1, y+1, 0 );
 				glEnd();
 
 				glBegin(GL_POLYGON);
-				glVertex3f( pieceX+x+1, pieceY+y+2, 0 );
-				glVertex3f( pieceX+x+1, pieceY+y+2, 1 );
-				glVertex3f( pieceX+x+1, pieceY+y+1, 1 );
-				glVertex3f( pieceX+x+1, pieceY+y+1, 0 );
+				glVertex3f( x+1, y+2, 0 );
+				glVertex3f( x+1, y+2, 1 );
+				glVertex3f( x+1, y+1, 1 );
+				glVertex3f( x+1, y+1, 0 );
 				glEnd();
 
 				glBegin(GL_POLYGON);
-				glVertex3f( pieceX+x+2, pieceY+y+2, 0 );
-				glVertex3f( pieceX+x+2, pieceY+y+2, 1 );
-				glVertex3f( pieceX+x+2, pieceY+y+1, 1 );
-				glVertex3f( pieceX+x+2, pieceY+y+1, 0 );
+				glVertex3f( x+2, y+2, 0 );
+				glVertex3f( x+2, y+2, 1 );
+				glVertex3f( x+2, y+1, 1 );
+				glVertex3f( x+2, y+1, 0 );
 				glEnd();
 
 				glBegin(GL_POLYGON);
-				glVertex3f( pieceX+x+2, pieceY+y+1, 1 );
-				glVertex3f( pieceX+x+2, pieceY+y+2, 1 );
-				glVertex3f( pieceX+x+1, pieceY+y+2, 1 );
-				glVertex3f( pieceX+x+1, pieceY+y+1, 1 );
+				glVertex3f( x+2, y+1, 1 );
+				glVertex3f( x+2, y+2, 1 );
+				glVertex3f( x+1, y+2, 1 );
+				glVertex3f( x+1, y+1, 1 );
 				glEnd();
 
 				glBegin(GL_POLYGON);
-				glVertex3f( pieceX+x+2, pieceY+y+1, 0 );
-				glVertex3f( pieceX+x+2, pieceY+y+1, 1 );
-				glVertex3f( pieceX+x+1, pieceY+y+1, 1 );
-				glVertex3f( pieceX+x+1, pieceY+y+1, 0 );
+				glVertex3f( x+2, y+1, 0 );
+				glVertex3f( x+2, y+1, 1 );
+				glVertex3f( x+1, y+1, 1 );
+				glVertex3f( x+1, y+1, 0 );
 				glEnd();
 
 				glBegin(GL_POLYGON);
-				glVertex3f( pieceX+x+2, pieceY+y+2, 0 );
-				glVertex3f( pieceX+x+2, pieceY+y+2, 1 );
-				glVertex3f( pieceX+x+1, pieceY+y+2, 1 );
-				glVertex3f( pieceX+x+1, pieceY+y+2, 0 );
+				glVertex3f( x+2, y+2, 0 );
+				glVertex3f( x+2, y+2, 1 );
+				glVertex3f( x+1, y+2, 1 );
+				glVertex3f( x+1, y+2, 0 );
+				glEnd();
+
+			}
+		}
+	}
+
+	glPopMatrix();
+
+}
+
+void drawPiecePreview() { // Draws a copy of the current piece at an approximation of the robot arms position (this is where the piece will be dropped)
+
+	armPieceX = round( ( (armLowerLength)*cos((armLowerAngle)*M_PI/180.0) ) + ( armUpperLength*cos((armLowerAngle+armUpperAngle)*M_PI/180.0) ) ) - 5;
+	armPieceY = round( ( (armLowerLength)*sin((armLowerAngle)*M_PI/180.0) ) + ( armUpperLength*sin((armLowerAngle+armUpperAngle)*M_PI/180.0) ) ) ;
+
+	for (int y = 0; y < 5; y++) {
+		for (int x = 0; x < 5; x++) {
+			if (currentPiece[y][x] != 0) {
+
+				if (boardP->detectCollision(currentPiece,armPieceY,armPieceX)) {
+					glColor3f(0.5, 0.5, 0.7);
+				} else {
+				switch(currentPiece[y][x]) { // Depending on the value of the current tile draw it with the color:
+					case 1: glColor3f(1.0, 0.0, 1.0); // Purple
+						break;
+					case 2: glColor3f(1.0, 0.0, 0.0); // Red
+						break;
+					case 3: glColor3f(1.0, 1.0, 0.0); // Yellow
+						break;
+					case 4: glColor3f(0.0, 1.0, 0.0); // Green
+						break;
+					case 5: glColor3f(1.0, 0.5, 0.0); // Orange
+						break;
+				}}
+
+				glBegin(GL_POLYGON);
+				glVertex3f( armPieceX+x+2, armPieceY+y+1, 0 );
+				glVertex3f( armPieceX+x+2, armPieceY+y+2, 0 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+2, 0 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+1, 0 );
+				glEnd();
+
+				glBegin(GL_POLYGON);
+				glVertex3f( armPieceX+x+1, armPieceY+y+2, 0 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+2, 1 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+1, 1 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+1, 0 );
+				glEnd();
+
+				glBegin(GL_POLYGON);
+				glVertex3f( armPieceX+x+2, armPieceY+y+2, 0 );
+				glVertex3f( armPieceX+x+2, armPieceY+y+2, 1 );
+				glVertex3f( armPieceX+x+2, armPieceY+y+1, 1 );
+				glVertex3f( armPieceX+x+2, armPieceY+y+1, 0 );
+				glEnd();
+
+				glBegin(GL_POLYGON);
+				glVertex3f( armPieceX+x+2, armPieceY+y+1, 1 );
+				glVertex3f( armPieceX+x+2, armPieceY+y+2, 1 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+2, 1 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+1, 1 );
+				glEnd();
+
+				glBegin(GL_POLYGON);
+				glVertex3f( armPieceX+x+2, armPieceY+y+1, 0 );
+				glVertex3f( armPieceX+x+2, armPieceY+y+1, 1 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+1, 1 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+1, 0 );
+				glEnd();
+
+				glBegin(GL_POLYGON);
+				glVertex3f( armPieceX+x+2, armPieceY+y+2, 0 );
+				glVertex3f( armPieceX+x+2, armPieceY+y+2, 1 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+2, 1 );
+				glVertex3f( armPieceX+x+1, armPieceY+y+2, 0 );
 				glEnd();
 
 			}
@@ -139,55 +265,55 @@ void drawPiece() { // Draws the current piece on the window, the current piece i
 
 }
 
-void drawArm() {
+void drawArm() { // Draw the parts of the robot arm
 
 	// LOWER ARM
 	glColor3f(0.2, 0.2, 0.2);
 	glPushMatrix();
 	glTranslatef(-2.5, 2, 0);
-	glRotatef(armLowerAngle, 0, 0, 1);
+	glRotatef(armLowerAngle-90, 0, 0, 1);
 	glTranslatef(2.5, -2, 0);
 
 	glBegin(GL_POLYGON);
-	glVertex3f( -2, 2, 0.2 );
-	glVertex3f( -2, 14, 0.2 );
-	glVertex3f( -3, 14, 0.2 );
-	glVertex3f( -3, 2, 0.2 );
+	glVertex3f( armLowerX+1, armLowerY, 0.2 );
+	glVertex3f( armLowerX+1, armLowerY+armLowerLength, 0.2 );
+	glVertex3f( armLowerX, armLowerY+armLowerLength, 0.2 );
+	glVertex3f( armLowerX, armLowerY, 0.2 );
 	glEnd();
 
 	glBegin(GL_POLYGON);
-	glVertex3f( -3, 14, 0.2 );
-	glVertex3f( -3, 14, 0.8 );
-	glVertex3f( -3, 2, 0.8 );
-	glVertex3f( -3, 2, 0.2 );
+	glVertex3f( armLowerX, armLowerY+armLowerLength, 0.2 );
+	glVertex3f( armLowerX, armLowerY+armLowerLength, 0.8 );
+	glVertex3f( armLowerX, armLowerY, 0.8 );
+	glVertex3f( armLowerX, armLowerY, 0.2 );
 	glEnd();
 
 	glBegin(GL_POLYGON);
-	glVertex3f( -2, 14, 0.2 );
-	glVertex3f( -2, 14, 0.8 );
-	glVertex3f( -2, 2, 0.8 );
-	glVertex3f( -2, 2, 0.2 );
+	glVertex3f( armLowerX+1, armLowerY+armLowerLength, 0.2 );
+	glVertex3f( armLowerX+1, armLowerY+armLowerLength, 0.8 );
+	glVertex3f( armLowerX+1, armLowerY, 0.8 );
+	glVertex3f( armLowerX+1, armLowerY, 0.2 );
 	glEnd();
 
 	glBegin(GL_POLYGON);
-	glVertex3f( -2, 2, 0.8 );
-	glVertex3f( -2, 14, 0.8 );
-	glVertex3f( -3, 14, 0.8 );
-	glVertex3f( -3, 2, 0.8 );
+	glVertex3f( armLowerX+1, armLowerY, 0.8 );
+	glVertex3f( armLowerX+1, armLowerY+armLowerLength, 0.8 );
+	glVertex3f( armLowerX, armLowerY+armLowerLength, 0.8 );
+	glVertex3f( armLowerX, armLowerY, 0.8 );
 	glEnd();
 
 	glBegin(GL_POLYGON);
-	glVertex3f( -2, 2, 0.2 );
-	glVertex3f( -2, 2, 0.8 );
-	glVertex3f( -3, 2, 0.8 );
-	glVertex3f( -3, 2, 0.2 );
+	glVertex3f( armLowerX+1, armLowerY, 0.2 );
+	glVertex3f( armLowerX+1, armLowerY, 0.8 );
+	glVertex3f( armLowerX, armLowerY, 0.8 );
+	glVertex3f( armLowerX, armLowerY, 0.2 );
 	glEnd();
 
 	glBegin(GL_POLYGON);
-	glVertex3f( -2, 14, 0.2 );
-	glVertex3f( -2, 14, 0.8 );
-	glVertex3f( -3, 14, 0.8 );
-	glVertex3f( -3, 14, 0.2 );
+	glVertex3f( armLowerX+1, armLowerY+armLowerLength, 0.2 );
+	glVertex3f( armLowerX+1, armLowerY+armLowerLength, 0.8 );
+	glVertex3f( armLowerX, armLowerY+armLowerLength, 0.8 );
+	glVertex3f( armLowerX, armLowerY+armLowerLength, 0.2 );
 	glEnd();
 
 		// UPPER ARM
@@ -198,45 +324,45 @@ void drawArm() {
 		glTranslatef(2.5, -14, 0);
 
 		glBegin(GL_POLYGON);
-		glVertex3f( -2, 14, 0.2 );
-		glVertex3f( -2, 24, 0.2 );
-		glVertex3f( -3, 24, 0.2 );
-		glVertex3f( -3, 14, 0.2 );
+		glVertex3f( armUpperX+1, armUpperY, 0.2 );
+		glVertex3f( armUpperX+1, armUpperY+armUpperLength, 0.2 );
+		glVertex3f( armUpperX, armUpperY+armUpperLength, 0.2 );
+		glVertex3f( armUpperX, armUpperY, 0.2 );
 		glEnd();
 
 		glBegin(GL_POLYGON);
-		glVertex3f( -3, 24, 0.2 );
-		glVertex3f( -3, 24, 0.8 );
-		glVertex3f( -3, 14, 0.8 );
-		glVertex3f( -3, 14, 0.2 );
+		glVertex3f( armUpperX, armUpperY+armUpperLength, 0.2 );
+		glVertex3f( armUpperX, armUpperY+armUpperLength, 0.8 );
+		glVertex3f( armUpperX, armUpperY, 0.8 );
+		glVertex3f( armUpperX, armUpperY, 0.2 );
 		glEnd();
 
 		glBegin(GL_POLYGON);
-		glVertex3f( -2, 24, 0.2 );
-		glVertex3f( -2, 24, 0.8 );
-		glVertex3f( -2, 14, 0.8 );
-		glVertex3f( -2, 14, 0.2 );
+		glVertex3f( armUpperX+1, armUpperY+armUpperLength, 0.2 );
+		glVertex3f( armUpperX+1, armUpperY+armUpperLength, 0.8 );
+		glVertex3f( armUpperX+1, armUpperY, 0.8 );
+		glVertex3f( armUpperX+1, armUpperY, 0.2 );
 		glEnd();
 
 		glBegin(GL_POLYGON);
-		glVertex3f( -2, 14, 0.8 );
-		glVertex3f( -2, 24, 0.8 );
-		glVertex3f( -3, 24, 0.8 );
-		glVertex3f( -3, 14, 0.8 );
+		glVertex3f( armUpperX+1, armUpperY, 0.8 );
+		glVertex3f( armUpperX+1, armUpperY+armUpperLength, 0.8 );
+		glVertex3f( armUpperX, armUpperY+armUpperLength, 0.8 );
+		glVertex3f( armUpperX, armUpperY, 0.8 );
 		glEnd();
 
 		glBegin(GL_POLYGON);
-		glVertex3f( -2, 14, 0.2 );
-		glVertex3f( -2, 14, 0.8 );
-		glVertex3f( -3, 14, 0.8 );
-		glVertex3f( -3, 14, 0.2 );
+		glVertex3f( armUpperX+1, armUpperY, 0.2 );
+		glVertex3f( armUpperX+1, armUpperY, 0.8 );
+		glVertex3f( armUpperX, armUpperY, 0.8 );
+		glVertex3f( armUpperX, armUpperY, 0.2 );
 		glEnd();
 
 		glBegin(GL_POLYGON);
-		glVertex3f( -2, 24, 0.2 );
-		glVertex3f( -2, 24, 0.8 );
-		glVertex3f( -3, 24, 0.8 );
-		glVertex3f( -3, 24, 0.2 );
+		glVertex3f( armUpperX+1, armUpperY+armUpperLength, 0.2 );
+		glVertex3f( armUpperX+1, armUpperY+armUpperLength, 0.8 );
+		glVertex3f( armUpperX, armUpperY+armUpperLength, 0.8 );
+		glVertex3f( armUpperX, armUpperY+armUpperLength, 0.2 );
 		glEnd();
 
 		glPopMatrix();
@@ -292,30 +418,40 @@ void drawArm() {
 
 void display(void) {
 
-	currentTime = glutGet(GLUT_ELAPSED_TIME);
+	currentTime = glutGet(GLUT_ELAPSED_TIME); // Update the current time
 	
+	/**
 	if (boardP->isGameOver()) { // Check if the game is over
 		Playing = false;
 	}
+	**/
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
-	gluLookAt(30.0f*sin(camAngle), 11.0f, 30.0f*cos(camAngle), 
-		6.0f, 11.0f, 0.0f, 
-		0.0f, 1.0f, 0.0f);
+	gluLookAt(35.0f*sin(camAngle), 11.0f, 35.0f*cos(camAngle), 6.0f, 11.0f, 0.0f, 0.0f, 1.0f, 0.0f); // Position the camera to look at the board depending on angle
 
 	boardP->drawBoard(); // Draws the tiles stored in the board
-	drawPiece(); // Draws the current piece
-	drawArm();
+	//drawPiece();
+
+	if (Playing) {	
+		drawPiecePreview();
+		drawArm();
+		drawTimer();
+	}
 
 	glutSwapBuffers();
 
 	if (Playing && (currentTime - lastTime) > timeInterval) {
 
-		lastTime = currentTime;
-				
-		pieceY--; // Drops the current piece down by 1 tile
+		lastTime = currentTime; // Update the previous time
+
+		if (boardP->detectCollision(currentPiece,armPieceY,armPieceX)) { // If the timer runs out and the piece is colliding then the game ends
+			Playing = false;
+		} else {
+			boardP->addPiece(currentPiece,armPieceY,armPieceX); // If the piece is not colliding, drop it down
+			newPiece = true; 
+		}
 
 		if (newPiece) { // If the flag to create a new piece is triggered then
 			clearPiece(); // The current piece is cleared
@@ -323,11 +459,15 @@ void display(void) {
 			newPiece = false; 
 		}
 
-		if (boardP->detectCollision(currentPiece,pieceY-1,pieceX)) { // Check if the current piece will collide the next time it drops down
-			boardP->addPiece(currentPiece,pieceY,pieceX); // The current piece is added to the board
-			newPiece = true; // A new piece must be created
-			boardP->deleteFinishedLines(); // Check and delete any filled lines
+		//pieceY--; 
+
+		/**
+		if (boardP->detectCollision(currentPiece,pieceY-1,pieceX)) { 
+			boardP->addPiece(currentPiece,pieceY,pieceX);
+			newPiece = true; 
+			boardP->deleteFinishedLines(); 
 		}
+		**/
 	}
 
 }
@@ -337,33 +477,38 @@ void pieceMovement(int key, int x, int y) { // For arrow key inputs
 	int modifierKey = glutGetModifiers();
 
 	switch(key) {			
-		case GLUT_KEY_LEFT: // Check if moving the piece will cause a collision, if not the piece is moved
+		case GLUT_KEY_LEFT: // Holding ctrl + left moves the camera to the left
 			if (modifierKey == GLUT_ACTIVE_CTRL) {
 				camAngle += 0.1;
 				if (camAngle >= 2*M_PI) camAngle -= 2*M_PI;
-			} else {
+			} 
+			/**
+			else {
 				if (!boardP->detectCollision(currentPiece,pieceY,pieceX-1)) pieceX--;
 			}
+			**/
 			break;
 
-		case GLUT_KEY_RIGHT: // Check if moving the piece will cause a collision, if not the piece is moved
+		case GLUT_KEY_RIGHT: // Holding ctrl + right moves the camera to the right
 			if (modifierKey == GLUT_ACTIVE_CTRL) {
 				camAngle -= 0.1;
 				if (camAngle >= 2*M_PI) camAngle -= 2*M_PI;
-			} else {
+			} 
+			/**
+			else {
 				if (!boardP->detectCollision(currentPiece,pieceY,pieceX+1)) pieceX++;
 			}
+			**/
 			break;
-
 		case GLUT_KEY_UP: 
 			createRotatedPiece(); // Create a temporary piece that is the current piece rotated CCW 90 degrees
-			if (!boardP->detectCollision(rotatedPiece,pieceY,pieceX)) { // If the rotated piece does not cause a collision
-				for (int y = 0; y < 5; y++) {
-					for (int x = 0; x < 5; x++) {
-						currentPiece[y][x] = rotatedPiece[y][x]; // The current piece is replaced with the rotated version
-					}
+			//if (!boardP->detectCollision(rotatedPiece,pieceY,pieceX)) {
+			for (int y = 0; y < 5; y++) {
+				for (int x = 0; x < 5; x++) {
+					currentPiece[y][x] = rotatedPiece[y][x]; // The current piece is replaced with the rotated version
 				}
 			}
+			//}
 			break;
 	}
 
@@ -376,12 +521,14 @@ void gameFunctions(unsigned char key, int x, int y) { // For letter key inputs
 			glutDestroyWindow(glutGetWindow()); // Closes the game
 			break;
 		case 'r':
-			boardP->clearBoard(); // Restartes the game
-			newPiece = true;
+			boardP->clearBoard(); // Restarts the game: board, timer and the current piece are reset
+			lastTime = currentTime;
+			clearPiece();
+			createPiece();
 			Playing = true;
 			break;
 		case 'a':
-			armLowerAngle += 5;
+			armLowerAngle += 5; // Change the angle of the arm
 			break;
 		case 'd':
 			armLowerAngle -= 5;
@@ -391,6 +538,16 @@ void gameFunctions(unsigned char key, int x, int y) { // For letter key inputs
 			break;
 		case 's':
 			armUpperAngle -= 5;
+			break;
+		case 32:
+			if (Playing) { // Pressing space will drop the current piece if it is not colliding with something
+				if (!boardP->detectCollision(currentPiece,armPieceY,armPieceX)) {
+					lastTime = currentTime;
+					boardP->addPiece(currentPiece,armPieceY,armPieceX);
+					clearPiece();
+					createPiece();
+				}
+			}
 			break;
 	}
 
@@ -411,7 +568,7 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowPosition(50, 100);
 	glutInitWindowSize(1600, 1600);
-	glutCreateWindow("Game");
+	glutCreateWindow("Assignment 2 - 3D Robot Arm Tetris");
 	
 	glEnable(GL_DEPTH_TEST);
 	glClearColor(1.0, 1.0, 1.0, 0.0);
